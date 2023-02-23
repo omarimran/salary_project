d0911b4a371b46176163c52d75a6587b2fccd068
Omar Kara Ali <omaromran.mn@gmail.com>
2023-02-23 20:51:45 +0300

    update

--- script.js
+++ script.js
@@ -7,30 +7,19 @@ const formatter = new Intl.NumberFormat("en-US", {
  minimumFractionDigits: 2,
  maximumFractionDigits: 2,
 });
-input.addEventListener("change", function (event) {
- if (!isNaN(input.value)) {
-  const formattedValue = formatter.format(input.value);
-  input.dataset.value = input.value;
-  input.value = formattedValue;
-  error_div.style.display = "none";
- }
- else {
-  input.value = "";
-  input.dataset.value = "";
- }
-});
 window.addEventListener("load", function () {
  input.focus();
 });
 calc_button.addEventListener("click", function () {
  result_div.style.opacity = 0;
  error_div.style.display = "none";
- if (!input.dataset.value || isNaN(input.dataset.value)) {
+ if (!input.value || input.value == 0) {
+  input.value = "";
   error_div.innerHTML = "ادخل الراتب التقاعدي";
   error_div.style.display = "block";
   return;
  }
- const salary = input.dataset.value;
+ const salary = input.value;
  const result = calculateY(salary);
  result_div.innerHTML = formatter.format(result);
  result_div.style.opacity = 1;
@@ -42,5 +31,6 @@ function calculateY(x) {
  x = x * 3;
  x = x + 7;
  x = x * 100000;
+ console.log(x);
  return x;
 }
